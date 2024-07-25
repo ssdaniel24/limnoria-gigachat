365697817ee01fce508567e93bcc91a84cb10472
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-07-25 23:58:44 +0300

    Added test for easiest command

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ from supybot.test import *
 class GigaChatTestCase(PluginTestCase):
     plugins = ('GigaChat',)
 
+    def testMsg(self):
+        self.assertNotError('msg Ты ведь нейросеть? Это так?')
+
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
